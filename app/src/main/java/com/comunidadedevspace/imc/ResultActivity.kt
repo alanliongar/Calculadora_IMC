c4758351fe5350368feb4e7980cb8c7ba6f97fed
alanliongar <alan.lucindo.gomes@alumni.usp.br>
2024-03-07 08:30:44 -0300

    Estilizando ambas as telas

--- app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
@@ -20,18 +20,22 @@ class ResultActivity : AppCompatActivity() {
         //tvResult.text = result.toString()
         result = (String.format("%.2f", result)).replace(",",".",false).toFloat()
         val Classi: String = if (result < 18.50) {
-            "Abaixo do peso"
+            "Abaixo do peso" //Azul claro
         } else if (result >= 18.50 && result < 25.00) {
-            "Peso normal"
+            "Peso normal" //Verde mediano
         } else if (result >= 25.00 && result < 30.00) {
-            "Sobrepeso"
+            "Sobrepeso" //Vermelho clarinho
         } else if (result >= 30.00 && result < 35.00) {
-            "Obesidade grau I"
+            "Obesidade grau I" //Laranja
         } else if (result >= 35.00 && result < 40.00) {
-            "Obesidade grau II"
+            "Obesidade grau II" //Laranja pra vermelho
         } else {
-            "Obesidade grau III"
+            "Obesidade grau III" //Vermelho escurão
         }
         tvClassificacao.text = Classi
+        //Tarefinha pra casa
+        //Criar a cor dinamica
+        //Criar o "70% abaixo e 30% acima" no textview da tela de resultado
+        //Criar o botão de limpar os dados, na primeira tela
     }
 }
