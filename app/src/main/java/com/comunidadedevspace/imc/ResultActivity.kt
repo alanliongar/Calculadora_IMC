1a4406d548105d68d2fba49aa6a11593d980f412
alanliongar <alan.lucindo.gomes@alumni.usp.br>
2024-03-08 06:06:51 -0300

    Criando_cores_dinamicas_e_atualizando_strings

--- app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
@@ -4,6 +4,7 @@ import android.icu.text.BidiClassifier
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -17,25 +18,26 @@ class ResultActivity : AppCompatActivity() {
         val tvResult = findViewById<TextView>(R.id.tv_result)
         val tvClassificacao = findViewById<TextView>(R.id.tv_classificacao)
         tvResult.text = String.format("%.2f", result)
-        //tvResult.text = result.toString()
+
         result = (String.format("%.2f", result)).replace(",",".",false).toFloat()
-        val Classi: String = if (result < 18.50) {
-            "Abaixo do peso" //Azul claro
-        } else if (result >= 18.50 && result < 25.00) {
-            "Peso normal" //Verde mediano
-        } else if (result >= 25.00 && result < 30.00) {
-            "Sobrepeso" //Vermelho clarinho
-        } else if (result >= 30.00 && result < 35.00) {
-            "Obesidade grau I" //Laranja
-        } else if (result >= 35.00 && result < 40.00) {
-            "Obesidade grau II" //Laranja pra vermelho
-        } else {
-            "Obesidade grau III" //Vermelho escurão
+        val Classi: Int = when (result) {
+            in 0.00f..18.49f -> R.string.class_1
+            in 18.50f..24.99f -> R.string.class_2
+            in 25.00f..29.99f -> R.string.class_3
+            in 30.00f..34.99f -> R.string.class_4
+            in 35.00f..39.99f -> R.string.class_5
+            else -> R.string.class_6
         }
-        tvClassificacao.text = Classi
-        //Tarefinha pra casa
-        //Criar a cor dinamica
-        //Criar o "70% abaixo e 30% acima" no textview da tela de resultado
-        //Criar o botão de limpar os dados, na primeira tela
+        tvClassificacao.text = getString(Classi)
+        val classificacaoColor = when (Classi){
+            R.string.class_1 -> R.color.blue
+            R.string.class_2 -> R.color.green
+            R.string.class_3 -> R.color.orange
+            R.string.class_4 -> R.color.red_light
+            R.string.class_5 -> R.color.orange_red
+            R.string.class_6 -> R.color.super_red
+            else -> R.color.black
+        }
+        tvClassificacao.setTextColor(ContextCompat.getColor(this,classificacaoColor))
     }
 }
